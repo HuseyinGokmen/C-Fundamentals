243e3c9b644cfa7a2c74ee846df8cdef1a0ced60
Huseyin Gokmen <88125418+HuseyinGokmen@users.noreply.github.com>
2024-01-24 18:01:57 +0300

    Add files via upload

---
+++ BooleanIfadeler.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	// true --> 1
+	// false --> 0
+	bool final;
+	int input;
+
+	cout << "bir input giriniz: " << "\n";
+	cin >> input;
+
+	// && and ve
+	// || or veya
+	final = (input <= 100) && (input >= 0);
+
+	cout << final << "\n";
+}
