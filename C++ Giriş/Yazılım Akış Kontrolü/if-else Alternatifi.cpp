104453ba5f16360e342fa6f6ee742b7aacbfc1ba
Huseyin Gokmen <88125418+HuseyinGokmen@users.noreply.github.com>
2024-01-24 23:47:25 +0300

    Add files via upload

---
+++ C++ Giriş/Yazılım Akış Kontrolü/if-else Alternatifi.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	/*
+	
+
+	int a = 3;
+	int b = 33;
+
+	if (a > b)
+	{
+		cout << "a, b'den buyuktur!";
+	}
+	else if (a < b)
+	{
+		cout << "a, b'den kucuktur!";
+	}
+
+	 // bunu daha kısa yoldan yapmak için  aşağıdaki satırları uygulayabiliriz < < < 
+	*/
+
+	int a = 3;
+	int b = 33;
+
+	string mesaj = (a > b) ? "a, b'den buyuk" : "a, b'den kucuk";  // condition'a bakıyor true ise soldakine - false ise sağdakine atıyor
+	cout << mesaj;
+
+}
