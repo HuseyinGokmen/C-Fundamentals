aed8fe6cd2ef2b70ef00d78c72cfc3bda9871726
Huseyin Gokmen <88125418+HuseyinGokmen@users.noreply.github.com>
2024-01-24 23:29:12 +0300

    Add files via upload

---
+++ if-else.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+void main() {
+
+	int input;
+
+	cout << " rakam giriniz: \n" << " ";
+	cin >> input;
+
+	if (input < 10 && input > -1)
+	{
+		cout << " bu bir rakamdir.\n";
+	}
+	else
+	{
+		cout << " bu bir rakam degildir!\n";
+	}
+}
