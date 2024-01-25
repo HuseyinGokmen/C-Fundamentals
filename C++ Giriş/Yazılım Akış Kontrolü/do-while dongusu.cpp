0f09cbfbd6faf87f70da0a2443b222d4f473c37f
Huseyin Gokmen <88125418+HuseyinGokmen@users.noreply.github.com>
2024-01-25 05:45:33 +0300

    Add files via upload

---
+++ C++ Giriş/Yazılım Akış Kontrolü/do-while dongusu.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	int sayi;
+	cout << "bir sayi giriniz: ";
+	cin >> sayi;
+
+
+	do
+	{
+		cout << "sayi: " << sayi << endl;
+		sayi--; 
+	} while (sayi > 0);   //false olsada 1 kez çalışır ve de koşul true olduğu müddetçe yukarısı çalışır v
+	//normalde sadece while döngüsünde false olsaydı aşağısı hiç işlenmezdi, ama do while'da false olsa da 1 kez çalışır
+	//yani do while'da önce yazdırır sonra koşul kontrolü olur.
+
+
+		cout << "pogram sonlandi \n";
+}
