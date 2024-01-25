86ba43390821ea236798aea76204a067759268ce
Huseyin Gokmen <88125418+HuseyinGokmen@users.noreply.github.com>
2024-01-25 05:10:44 +0300

    Add files via upload

---
+++ C++ Giriş/Yazılım Akış Kontrolü/While Dongusu.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+void main() {
+
+	int sayi;
+	cout << "bir sayi giriniz--> ";
+	cin >> sayi;
+
+	while (sayi > 0)   //koşul true olduğu müddetçe devam eder 
+	{
+		cout << "donence! --> " << sayi << "\n";
+
+		sayi--;
+
+		if (sayi == 5)  //while true olsada kod her seferinde bu koşulu da kontrol edecek, ve break gördüğü an duracak.
+		{
+			break;
+		}
+	}
+	cout << "donence bitti (:" << endl;;
+}
