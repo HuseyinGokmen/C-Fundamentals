0f8f5b0baae9dd667bc03daacc3c082d735749a5
Huseyin Gokmen <88125418+HuseyinGokmen@users.noreply.github.com>
2024-01-25 04:53:14 +0300

    Add files via upload

---
+++ C++ Giriş/Yazılım Akış Kontrolü/switch-case2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char harfNotu;
+    cout << "Harf notunuzu giriniz: ";
+
+    cin >> harfNotu;
+
+	switch (harfNotu)
+	{
+	case 'a':
+	case 'A':
+		cout << "Mukemmel bir harf notu!\n";
+		break;
+
+	case 'b':
+	case 'B':
+		cout << "Iyi bir harf notu!\n";
+		break;
+
+	case 'c':
+	case 'C':
+		cout << "Idare eder! \n";
+		break;
+
+	case 'd':
+	case 'D':
+		cout << "Kalmaktan iyi!\n";
+		break;
+
+
+	case 'f':
+	case 'F':
+		cout << "Daha cok calismalisin!\n";
+		break;
+
+	default:
+		cout << "Lutfen gecerli bir harf notu giriniz!\n";
+		break;
+	}
+	cout << "Program sonlandi.\n";
+}
