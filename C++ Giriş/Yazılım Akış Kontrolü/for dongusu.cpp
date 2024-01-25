afb98d332594947b6169de2b544ffcfbdc4c31e9
Huseyin Gokmen <88125418+HuseyinGokmen@users.noreply.github.com>
2024-01-25 16:24:21 +0300

    Add files via upload

---
+++ C++ Giriş/Yazılım Akış Kontrolü/for dongusu.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	cout << "Bir sayi giriniz: ";
+
+	int i;
+
+	cin >> i;
+
+	for (i; i >= -5; i--)  //başangıç değeri , koşulu, ve (koşula göre)arttırma/azaltma
+	{
+		cout << "i: " << i << "\n";
+
+		if (i == 0)
+		{
+			break;
+		}
+	}
+
+	cout << "Kod sonlandi.\n";
+
+}
