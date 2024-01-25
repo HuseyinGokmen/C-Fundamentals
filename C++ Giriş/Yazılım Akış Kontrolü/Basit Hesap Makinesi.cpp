d02cc0985a04e26e205788b4175d5c387aa65671
Huseyin Gokmen <88125418+HuseyinGokmen@users.noreply.github.com>
2024-01-25 19:38:50 +0300

    Add files via upload

---
+++ C++ Giriş/Yazılım Akış Kontrolü/Basit Hesap Makinesi.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sayi1, sayi2;
+	char secim;
+	char devam;
+
+	while (1)
+	{
+		cout << "Lutfen matematik isleminizi girin: ";
+		cin >> sayi1 >> secim >> sayi2;
+
+		switch (secim)
+		{
+
+		case '+':
+			cout << "Sonuc: " << sayi1 + sayi2 << endl;
+
+			break;
+
+		case '-':
+			cout << "Sonuc: " << sayi1 - sayi2 << endl;
+			break;
+
+		case '*':
+			cout << "Sonuc: " << sayi1 * sayi2 << endl;
+			break;
+
+		case '/':
+			cout << "Sonuc: " << sayi1 / sayi2 << endl;
+			break;
+
+		default:
+			cout << "Yanlis islem \n";
+		}
+
+		cout << "Islem yapmaya devam etmek istiyorsanız 'y' tusuna basiniz --> ";
+		cin >> devam;
+		if (devam != 'y')
+		{
+			break;
+		}
+
+	}
+
+}
